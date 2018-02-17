f08bb99ab75c5765209ccf30bca672864ce57f23
Jack Lord <jack.lord@opendataservices.coop>
2018-02-17 14:42:32 +0000

    basic BODS test fixture

--- tests/test_bodsutils.py
+++ tests/test_bodsutils.py
@@ -4,22 +4,64 @@
 """Tests for `bodsutils` package."""
 
 import pytest
+import json
+from jsonschema import Draft4Validator
 
 
 from bodsutils import bodsutils
 
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(3) == 4
+
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+def bods_schema():
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    BODS schema fixture.
+    Version is currently dependent on the submodule checked out.
+    """
+    with open("./data-standard/schema/beneficial-ownership-statements.json", 'r') as f:
+    	schema = json.load(f)
+    return schema
 
+@pytest.fixture
+def valid_bods():
+	"""Minimal valid BODS"""
+	bods = {
+	        "statementGroups":
+	          [
+	            {
+	              "id": "265fad50-ac77-4703-9aa6-0e6523f23767",
+	              "entityStatements":
+	              [
+	              ],
+	              "beneficialOwnershipStatements":
+	              [
+	                {
+	                  "id": "23569c9b-2f99-456f-a22a-23459e66a31f",
+	                  "identifiers":
+	                  [
+	                    {
+	                      "scheme": "GB-COH",
+	                      "id": "00990099"
+	                    }
+	                  ]
+	                }
+	              ]
+	            }
+	          ]
+	        }
+	return bods
 
-def test_content(response):
+def test_valid_bods(valid_bods, bods_schema):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    v = Draft4Validator(bods_schema)
+    for error in sorted(v.iter_errors(valid_bods), key=str):
+    	print(error.message)
+    assert v.is_valid(valid_bods)
